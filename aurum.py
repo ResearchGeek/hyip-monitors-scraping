a09ee1a9e503d0a23d62ee2f33b7ef7296643648
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-02-21 00:50:24 +0100

    Stub of the program in aurum.py

---
+++ aurum.py
@@ -0,0 +1,119 @@
+# coding=UTF-8
+'''
+Downloads data from popular hyip monitors
+
+@version 1.0 Midnight
+
+Midnight is a leisure planet visited by the Doctor and Donna in the episode of the same name.
+It has golden spas, anti-gravity restaurants, sapphire waterfalls, and a landscape of diamonds.
+The planet's sun emits x-tonic radiation, which vaporises organic matter and can only be viewed
+safely through sufficiently thick finito glass. The radiation poisons the diamonds, so the planet's
+surface can never be touched.
+
+@author Oskar Jarczyk
+@since 1.0
+@update 21.02.2014
+'''
+
+version_name = 'version 1.0 codename: Midnight'
+pull_request_filename = 'hyip.csv'
+
+import csv
+import scream
+import codecs
+import cStringIO
+import os
+import sys
+import urllib
+import urllib2
+import simplejson
+import hashlib
+import mechanize
+import time
+from bs4 import BeautifulSoup
+import unicodedata
+
+
+class MyDialect(csv.Dialect):
+    strict = True
+    skipinitialspace = True
+    quoting = csv.QUOTE_MINIMAL
+    delimiter = ','
+    escapechar = '\\'
+    quotechar = '"'
+    lineterminator = '\n'
+
+
+class ReadDialect(csv.Dialect):
+    strict = True
+    skipinitialspace = False
+    quoting = csv.QUOTE_NONE
+    delimiter = ','
+    escapechar = '\\'
+    lineterminator = '\n'
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=ReadDialect, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=MyDialect, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+if __name__ == "__main__":
+    scream.say('Start main execution')
+    scream.say(version_name)
---
+++ utils.py
@@ -0,0 +1,29 @@
+import unicodedata
+
+latin_letters = {}
+symbols = (u"абвгдеёзийклмнопрстуфхъыьэАБВГДЕЁЗИЙКЛМНОПРСТУФХЪЫЬЭ",
+           u"abvgdeezijklmnoprstufh'y'eABVGDEEZIJKLMNOPRSTUFH'Y'E")
+#tr = {ord(a): ord(b) for a, b in zip(*symbols)}
+possible_translation = dict()
+#moving to python 2.7.3
+for a in zip(*symbols):
+    for b in zip(*symbols):
+        possible_translation.update({ord(a[0]): ord(b[1])})
+
+
+def cyrillic2latin(input):
+    return input.translate(possible_translation)
+
+
+def is_latin(uchr):
+    try:
+        return latin_letters[uchr]
+    except KeyError:
+        return latin_letters.setdefault(uchr,
+                                        'LATIN' in unicodedata.name(uchr))
+
+
+def only_roman_chars(unistr):
+    return all(is_latin(uchr)
+               for uchr in unistr
+               if uchr.isalpha())
